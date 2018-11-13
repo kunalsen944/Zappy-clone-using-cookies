b8c26d1b237a1dfad84dc746bf5e49deb5b82a3a
Kunal Sen <kunalsen944@gmail.com>
2018-11-13 08:41:46 +0530

    Zappy on Heroku

--- zappyproject/zappyproject/settings.py
+++ zappyproject/zappyproject/settings.py
@@ -77,10 +77,9 @@ WSGI_APPLICATION = 'zappyproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+      )
 }
 
 
